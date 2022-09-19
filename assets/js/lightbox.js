cc8b1495dcdfe436e95f590eb1fa31403ac32ef6
waflol <lenguyenhunganh2001@gmail.com>
2022-09-20 02:10:02 +0700

    update

---
+++ assets/js/lightbox.js
@@ -0,0 +1,7 @@
+const lightbox = GLightbox({
+    'href': 'https://youtu.be/ZSt9tm3RoUU',
+    'type': 'video',
+    'source': 'youtube', //vimeo, youtube or local
+    'width': 900,
+    'autoPlayVideos':'true',
+});
